0c1014859529067037d9ba626bf4d7665d872b33
sahil <sasulkarsahil491@gmail.com>
2025-12-18 23:01:32 +0530

    Fix CSS file name case for Linux build

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import IndicatorCard from "../ui/IndicatorCard";
-import "./dashboardUI.css";   // this must match file name
+import "./DashboardUI.css";   // this must match file name
 
 export default function Dashboard(){
 
